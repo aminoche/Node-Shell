34155ad30866acbb945f49407822048124cbef97
Gittie Atlas <gittieatlas@gmail.com>
2018-07-31 12:34:28 -0400

    pwd and ls commands

--- bash.js
+++ bash.js
@@ -1,3 +1,6 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
+
 //output a prompt
 process.stdout.write('prompt > ');
 
@@ -5,9 +8,11 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
   if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
   } else {
     process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
   }
-  process.stdout.write('\nprompt > ');
 });
